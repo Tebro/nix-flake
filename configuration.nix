b7fb149b4079a1e87d8aee51f4bd880d2350ffd3
Tebro <tebro@git.simplelogin.com>
2025-04-12 00:12:46 +0300

    Include unstable for neovim

--- configuration.nix
+++ configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, pkgs-unstable, ... }:
 
 {
   imports =
@@ -84,7 +84,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    neovim
+    pkgs-unstable.neovim
     wget
     git
     gnumake
--- flake.nix
+++ flake.nix
@@ -3,16 +3,29 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, nixpkgs-unstable, ... }@inputs: 
+    let 
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
+    in 
+    {
 
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-	system = "x86_64-linux";
-	modules = [
-		./configuration.nix
-	];
+
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+          	./configuration.nix
+          ];
+          specialArgs = {
+	    inherit pkgs;
+            inherit pkgs-unstable;
+          };
+      };
+
     };
-
-  };
 }
