477c37b71f732e7f018d63badf59f706685efc2b
Tebro <git@tebro.simplelogin.com>
2025-04-12 23:18:04 +0300

    Improve nvim config

--- configuration.nix
+++ configuration.nix
@@ -1,125 +1,133 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, pkgs-unstable, ... }:
-
 {
-	imports =
-		[ # Include the results of the hardware scan.
-			./hardware-configuration.nix
-			./system/boot.nix
-			./system/time.nix
-			./system/lang.nix
-			./system/hyprland.nix
-		];
+  config,
+  pkgs,
+  pkgs-unstable,
+  ...
+}: {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ./system/boot.nix
+    ./system/time.nix
+    ./system/lang.nix
+    ./system/hyprland.nix
+  ];
 
+  nix.settings.experimental-features = ["nix-command" "flakes"];
 
-	nix.settings.experimental-features = ["nix-command" "flakes"];
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-	networking.hostName = "nixos"; # Define your hostname.
-	# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-	# Configure network proxy if necessary
-	# networking.proxy.default = "http://user:password@proxy:port/";
-	# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  # Enable networking
+  networking.networkmanager.enable = true;
 
-	# Enable networking
-	networking.networkmanager.enable = true;
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.richard = {
+    isNormalUser = true;
+    description = "richard";
+    extraGroups = ["networkmanager" "wheel"];
+    packages = with pkgs; [];
+  };
 
-	# Define a user account. Don't forget to set a password with ‘passwd’.
-	users.users.richard = {
-		isNormalUser = true;
-		description = "richard";
-		extraGroups = [ "networkmanager" "wheel" ];
-		packages = with pkgs; [];
-	};
+  users.users.tebro = {
+    isNormalUser = true;
+    description = "tebro";
+    extraGroups = ["networkmanager"];
+    packages = with pkgs; [];
+  };
 
-	users.users.tebro = {
-		isNormalUser = true;
-		description = "tebro";
-		extraGroups = [ "networkmanager" ];
-		packages = with pkgs; [];
-	};
+  security.sudo = {
+    enable = true;
+    extraRules = [
+      {
+        users = ["tebro"];
+        commands = [
+          {
+            command = "ALL";
+            options = ["NOPASSWD"];
+          }
+        ];
+      }
+      {
+        users = ["richard"];
+        commands = [
+          {
+            command = "ALL";
+            options = ["NOPASSWD"];
+          }
+        ];
+      }
+    ];
+  };
+  security.polkit.enable = true;
 
-	security.sudo = {
-		enable = true;
-		extraRules = [
-		{ users = ["tebro"];
-			commands = [
-				{command = "ALL"; options = ["NOPASSWD"];}
-			];
-		}
-		{ users = ["richard"];
-			commands = [
-				{command = "ALL"; options = ["NOPASSWD"];}
-			];
-		}
-		];
-	};
-	security.polkit.enable = true;
+  environment.variables.EDITOR = "nvim";
 
-	environment.variables.EDITOR = "nvim";
+  # Enable automatic login for the user.
+  #services.getty.autologinUser = "richard";
 
-	# Enable automatic login for the user.
-	#services.getty.autologinUser = "richard";
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
-	# Allow unfree packages
-	nixpkgs.config.allowUnfree = true;
+  #sound.enable = true;
+  #security.rtkit.enable = true;
+  #services.pipewire = {
+  #  enable = true;
+  #  alsa.enable = true;
+  #  alsa.support32Bit = true;
+  #  pulse.enable = true;
+  #  jack.enable = true;
+  #};
 
-	#sound.enable = true;
-	#security.rtkit.enable = true;
-	#services.pipewire = {
-	#  enable = true;
-	#  alsa.enable = true;
-	#  alsa.support32Bit = true;
-	#  pulse.enable = true;
-	#  jack.enable = true;
-	#};
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    pkgs-unstable.neovim
+    bat
+    wget
+    git
+    gnumake
+    gcc
+    btop
+    ripgrep
+    xfce.thunar
+    playerctl
+    tmux
+    home-manager
+  ];
 
-	# List packages installed in system profile. To search, run:
-	# $ nix search wget
-	environment.systemPackages = with pkgs; [
-		pkgs-unstable.neovim
-		bat
-		wget
-		git
-		gnumake
-		gcc
-		btop
-		xfce.thunar
-		playerctl
-		tmux
-		home-manager
-	];
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
 
+  # List services that you want to enable:
 
-	# Some programs need SUID wrappers, can be configured further or are
-	# started in user sessions.
-	# programs.mtr.enable = true;
-	# programs.gnupg.agent = {
-	#   enable = true;
-	#   enableSSHSupport = true;
-	# };
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+  services.dbus.enable = true;
 
-	# List services that you want to enable:
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = false;
 
-	# Enable the OpenSSH daemon.
-	services.openssh.enable = true;
-	services.dbus.enable = true;
-
-	# Open ports in the firewall.
-	# networking.firewall.allowedTCPPorts = [ ... ];
-	# networking.firewall.allowedUDPPorts = [ ... ];
-	# Or disable the firewall altogether.
-	networking.firewall.enable = false;
-
-	# This value determines the NixOS release from which the default
-	# settings for stateful data, like file locations and database versions
-	# on your system were taken. It‘s perfectly fine and recommended to leave
-	# this value at the release version of the first install of this system.
-	# Before changing this value read the documentation for this option
-	# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-	system.stateVersion = "24.11"; # Did you read the comment?
-
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
 }
--- home/user/nvf.nix
+++ home/user/nvf.nix
@@ -9,25 +9,74 @@
           mapLeader = "space";
         };
         options = {
-          tabstop = 4;
-          shiftwidth = 4;
+          tabstop = 2;
+          shiftwidth = 2;
+          softtabstop = 2;
+          smartindent = true;
+          ignorecase = true;
+          smartcase = true;
+          incsearch = true;
+          signcolumn = "yes";
+          termguicolors = true;
+          scrolloff = 8;
+          relativenumber = true;
+        };
+
+        diagnostics = {
+          config.virtual_text = true;
+          nvim-lint.enable = true;
         };
 
         lsp = {
           enable = true;
           formatOnSave = true;
           inlayHints.enable = true;
+          mappings = {
+            codeAction = "<leader>ca";
+            goToDefinition = "gd";
+            renameSymbol = "<leader>rn";
+          };
         };
 
-        telescope.enable = true;
+        autocomplete.nvim-cmp = {
+          enable = true;
+        };
+
+        telescope = {
+          enable = true;
+        };
 
         git.enable = true;
 
-        languages.nix = {
-          enable = true;
-          lsp.enable = true;
-          treesitter.enable = true;
-          format.enable = true;
+        languages = {
+          nix = {
+            enable = true;
+            lsp.enable = true;
+            treesitter.enable = true;
+            format.enable = true;
+          };
+          ts = {
+            enable = true;
+            lsp.enable = true;
+            treesitter.enable = true;
+          };
+          tailwind = {
+            enable = true;
+            lsp.enable = true;
+          };
+        };
+
+        utility = {
+          oil-nvim.enable = true;
+          motion.flash-nvim = {
+            enable = true;
+            mappings = {
+              jump = "zk";
+              treesitter = "zK";
+              remote = "r";
+              treesitter_search = "R";
+            };
+          };
         };
 
         treesitter = {
@@ -35,16 +84,81 @@
         };
 
         keymaps = [
+          # System clipboard
+          {
+            mode = ["n" "v"];
+            key = "<leader>y";
+            action = "\"+y";
+          }
           {
             mode = "n";
-            key = "<leader>ff";
-            action = "require('telescope.builtin').find_files";
-            lua = true;
+            key = "<leader>p";
+            action = "\"+p";
           }
+          # Move lines
+          {
+            mode = "v";
+            key = "J";
+            action = ">+1<CR>gv=gv";
+          }
+          {
+            mode = "v";
+            key = "K";
+            action = "<-2<CR>gv=gv";
+          }
+          # Git
           {
             mode = "n";
             key = "<leader>gs";
             action = ":Git<CR>";
+          }
+          # Write util
+          {
+            mode = "n";
+            key = "<leader>w";
+            action = "vim.cmd.update";
+            lua = true;
+          }
+          # Oil
+          {
+            mode = "n";
+            key = "<leader>e";
+            action = "function() vim.cmd.edit(vim.fn.getcwd())end";
+            lua = true;
+          }
+          {
+            mode = "n";
+            key = "<leader>E";
+            action = "vim.cmd.Oil";
+            lua = true;
+          }
+          # Maintain center while scrolling
+          {
+            mode = "n";
+            key = "<c-d>";
+            action = "<c-d>zz";
+          }
+          {
+            mode = "n";
+            key = "<c-u>";
+            action = "<c-u>zz";
+          }
+          # Clear highlights
+          {
+            mode = "n";
+            key = "<leader><return>";
+            action = ":noh<cr>";
+          }
+          {
+            mode = "n";
+            key = "<leader>tT";
+            action = ":tabnew +terminal<cr>";
+          }
+          # Maximise window
+          {
+            mode = "n";
+            key = "<C-W>m";
+            action = "<C-W>|<C-W>_";
           }
         ];
       };
