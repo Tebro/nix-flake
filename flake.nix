b0ce687e36f2c338f9be7a95f9341fa568a27a45
Tebro <git@tebro.simplelogin.com>
2025-04-13 00:26:35 +0300

    Formatting and cleaning

--- flake.nix
+++ flake.nix
@@ -1,46 +1,49 @@
 {
-	description = "Tebros first flake";
+  description = "Tebros first flake";
 
-	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-		nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-		home-manager.url = "github:nix-community/home-manager/release-24.11";
-		home-manager.inputs.nixpkgs.follows = "nixpkgs";
-		nvf.url = "github:notashelf/nvf";
-	};
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nvf.url = "github:notashelf/nvf";
+  };
 
-	outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, nvf, ... }@inputs: 
-		let 
-			lib = nixpkgs.lib;
-			system = "x86_64-linux";
-			pkgs = nixpkgs.legacyPackages.${system};
-			pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
-		in 
-			{
-
-
-			nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-				inherit system;
-				modules = [
-					./configuration.nix
-				];
-				specialArgs = {
-					inherit pkgs;
-					inherit pkgs-unstable;
-				};
-			};
-			homeConfigurations = {
-				tebro = home-manager.lib.homeManagerConfiguration {
-					inherit pkgs;
-					modules = [ 
-						nvf.homeManagerModules.default
-						./home 
-					];
-					extraSpecialArgs = {
-						inherit pkgs-unstable;
-						inherit inputs;
-					};
-				};
-			};
-		};
+  outputs = {
+    self,
+    nixpkgs,
+    nixpkgs-unstable,
+    home-manager,
+    nvf,
+    ...
+  } @ inputs: let
+    lib = nixpkgs.lib;
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
+  in {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        ./configuration.nix
+      ];
+      specialArgs = {
+        inherit pkgs;
+        inherit pkgs-unstable;
+      };
+    };
+    homeConfigurations = {
+      tebro = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [
+          nvf.homeManagerModules.default
+          ./home
+        ];
+        extraSpecialArgs = {
+          inherit pkgs-unstable;
+          inherit inputs;
+        };
+      };
+    };
+  };
 }
--- home/user/git.nix
+++ home/user/git.nix
@@ -1,36 +1,32 @@
-let
-  userName = "tebro";
-  email = "git@tebro.simplelogin.com";
-in
 {
-    programs.git = {
-        enable = true;
-        userName = userName;
-        userEmail = email;
+  programs.git = {
+    enable = true;
+    userName = "tebro";
+    userEmail = "git@tebro.simplelogin.com";
 
-	aliases = {
-		s = "status"; 
-		c = "checkout";
-		cam = "commit -am";
-		ce = "commit --amend --no-edit";
-		l = "log --oneline --all --graph --decorate";
-		l1 = "log --oneline --graph --decorate";
-		r = "rebase";
-		ri = "rebase -i";
-		f = "fetch";
-		cm = "commit -m";
-	};
-	extraConfig = {
-		pull = {
-			rebase = true;
-		};
-		push = {
-			default = "current";
-		};
-		rebase = {
-			autosquash = true;
-			autostash = true;
-		};
-	};
+    aliases = {
+      s = "status";
+      c = "checkout";
+      cam = "commit -am";
+      ce = "commit --amend --no-edit";
+      l = "log --oneline --all --graph --decorate";
+      l1 = "log --oneline --graph --decorate";
+      r = "rebase";
+      ri = "rebase -i";
+      f = "fetch";
+      cm = "commit -m";
     };
+    extraConfig = {
+      pull = {
+        rebase = true;
+      };
+      push = {
+        default = "current";
+      };
+      rebase = {
+        autosquash = true;
+        autostash = true;
+      };
+    };
+  };
 }
