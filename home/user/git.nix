3f0ff2f597edac190a966fd9a95a39058cfa3908
Tebro <git@tebro.simplelogin.com>
2025-04-12 15:26:31 +0300

    Git config

--- home/user/git.nix
+++ home/user/git.nix
@@ -7,5 +7,30 @@ in
         enable = true;
         userName = userName;
         userEmail = email;
+
+	aliases = {
+		s = "status"; 
+		c = "checkout";
+		cam = "commit -am";
+		ce = "commit --amend --no-edit";
+		l = "log --oneline --all --graph --decorate";
+		l1 = "log --oneline --graph --decorate";
+		r = "rebase";
+		ri = "rebase -i";
+		f = "fetch";
+		cm = "commit -m";
+	};
+	extraConfig = {
+		pull = {
+			rebase = true;
+		};
+		push = {
+			default = "current";
+		};
+		rebase = {
+			autosquash = true;
+			autostash = true;
+		};
+	};
     };
 }
