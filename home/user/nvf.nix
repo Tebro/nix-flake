40118f924a8b4c25c1c7ede259f2c071c77bba81
Tebro <git@tebro.simplelogin.com>
2025-04-12 16:07:35 +0300

    Add more vim config

--- home/user/nvf.nix
+++ home/user/nvf.nix
@@ -1,16 +1,53 @@
 {
-	programs.nvf = {
-		enable = true;
-		settings = {
-                        vim.vimAlias = true;
-			vim.lsp = {
-				enable = true;
-			};
-                        vim.languages.nix = {
-                                enable = true;
-                                lsp.enable = true;
-                                treesitter.enable = true;
-                        };
-		};
-	};
+  programs.nvf = {
+    enable = true;
+    settings = {
+      vim = {
+        vimAlias = true;
+
+        globals = {
+          mapLeader = "space";
+        };
+        options = {
+          tabstop = 4;
+          shiftwidth = 4;
+        };
+
+        lsp = {
+          enable = true;
+          formatOnSave = true;
+          inlayHints.enable = true;
+        };
+
+        telescope.enable = true;
+
+        git.enable = true;
+
+        languages.nix = {
+          enable = true;
+          lsp.enable = true;
+          treesitter.enable = true;
+          format.enable = true;
+        };
+
+        treesitter = {
+          enable = true;
+        };
+
+        keymaps = [
+          {
+            mode = "n";
+            key = "<leader>ff";
+            action = "require('telescope.builtin').find_files";
+            lua = true;
+          }
+          {
+            mode = "n";
+            key = "<leader>gs";
+            action = ":Git<CR>";
+          }
+        ];
+      };
+    };
+  };
 }
