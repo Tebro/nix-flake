868078e366fb2b41de1af5df10560edf5f6e2f6a
Tebro <git@tebro.simplelogin.com>
2025-04-12 15:50:32 +0300

    Add basic nix LSP

--- home/user/nvf.nix
+++ home/user/nvf.nix
@@ -2,9 +2,15 @@
 	programs.nvf = {
 		enable = true;
 		settings = {
+                        vim.vimAlias = true;
 			vim.lsp = {
 				enable = true;
 			};
+                        vim.languages.nix = {
+                                enable = true;
+                                lsp.enable = true;
+                                treesitter.enable = true;
+                        };
 		};
 	};
 }
