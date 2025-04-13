7a690dd4ed2edadfbd7dd6c8f6bec3efcca6b037
tebro <git@tebro.simplelogin.com>
2025-04-13 18:03:40 +0300

    Format

--- system/hyprland.nix
+++ system/hyprland.nix
@@ -1,24 +1,27 @@
-{ config, pkgs, lib, ... }:
 {
-	programs.hyprland = {
-		enable = true;
-		xwayland.enable = true;
-	};
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
 
-	environment.systemPackages = with pkgs; [
-		waybar
-			rofi-wayland
-			dunst
-			swww
-			kitty
-			ghostty
-	];
+  environment.systemPackages = with pkgs; [
+    waybar
+    rofi-wayland
+    dunst
+    swww
+    kitty
+    ghostty
+  ];
 
-	xdg.portal.enable = true;
-	xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-gtk];
 
-	fonts.packages = with pkgs; [
-		nerdfonts
-	];
-
+  fonts.packages = with pkgs; [
+    nerdfonts
+  ];
 }
