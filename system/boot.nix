46ff5e0e5a9f1d171376b05f9dc038ee566659bf
Tebro <git@tebro.simplelogin.com>
2025-04-13 11:19:09 +0300

    New kernel

--- system/boot.nix
+++ system/boot.nix
@@ -1,6 +1,11 @@
-{ config, pkgs, lib, ... }:
 {
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 }
