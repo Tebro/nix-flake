a71eddac275c1f7387a3b928a37a698df111994b
Richard Weber <git@tebro.simplelogin.com>
2025-04-14 20:06:42 +0300

    Add nixos hardware config

--- flake.nix
+++ flake.nix
@@ -8,12 +8,14 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nvf.url = "github:notashelf/nvf";
     catppuccin.url = "github:catppuccin/nix";
+		nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
   outputs = {
     self,
     nixpkgs,
     nixpkgs-unstable,
+		nixos-hardware,
     home-manager,
     nvf,
     catppuccin,
@@ -38,6 +40,7 @@
       inherit system;
       modules = [
         ./hosts/hornet.nix
+				nixos-hardware.nixosModules.lenovo-thinkpad-e14-intel
       ];
       specialArgs = {
         inherit inputs;
