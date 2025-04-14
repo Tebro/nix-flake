785efdaadf5ab44291f07fc0e4fd6ecd4038144c
tebro <git@tebro.simplelogin.com>
2025-04-14 17:59:19 +0300

    Update system

--- system/nixsettings.nix
+++ system/nixsettings.nix
@@ -13,11 +13,11 @@
       experimental-features = ["nix-command" "flakes"];
       auto-optimise-store = true;
       substituters = [
-        "https://hyprland.cachix.org"
+        #"https://hyprland.cachix.org"
         #"https://nix-gaming.cachix.org"
       ];
       trusted-public-keys = [
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        #"hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         #"nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
       ];
     };
