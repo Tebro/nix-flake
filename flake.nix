0f5a4569abdf0e825c8a35a11ab85c1c96542de0
tebro <git@tebro.simplelogin.com>
2025-04-28 17:23:32 +0300

    Formatting

--- flake.nix
+++ flake.nix
@@ -7,71 +7,50 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     catppuccin.url = "github:catppuccin/nix";
     catppuccin.inputs.nixpkgs.follows = "nixpkgs";
-		openaws-vpn-client.url = "github:jonathanxD/openaws-vpn-client";
+    openaws-vpn-client.url = "github:jonathanxD/openaws-vpn-client";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    home-manager,
-		catppuccin,
-		openaws-vpn-client,
-    ...
-  } @ inputs: {
-    nixosConfigurations.nixosvm = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        catppuccin.nixosModules.catppuccin
-        ./hosts/nixosvm.nix
-      ];
-      specialArgs = {
-        inherit inputs;
+  outputs = { self, nixpkgs, home-manager, catppuccin, openaws-vpn-client, ...
+    }@inputs: {
+      nixosConfigurations.nixosvm = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ catppuccin.nixosModules.catppuccin ./hosts/nixosvm.nix ];
+        specialArgs = { inherit inputs; };
       };
-    };
-    nixosConfigurations.hornet = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        catppuccin.nixosModules.catppuccin
-        home-manager.nixosModules.home-manager
-        ./hosts/hornet.nix
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.backupFileExtension = "backup";
-          home-manager.users.tebro = {
-            imports = [
-              ./home
-              catppuccin.homeModules.catppuccin
-            ];
-          };
-        }
-      ];
-      specialArgs = {
-        inherit inputs;
+      nixosConfigurations.hornet = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          catppuccin.nixosModules.catppuccin
+          home-manager.nixosModules.home-manager
+          ./hosts/hornet.nix
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "backup";
+            home-manager.users.tebro = {
+              imports = [ ./home catppuccin.homeModules.catppuccin ];
+            };
+          }
+        ];
+        specialArgs = { inherit inputs; };
       };
-    };
-    nixosConfigurations.raptor = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        catppuccin.nixosModules.catppuccin
-        home-manager.nixosModules.home-manager
-        ./hosts/raptor.nix
+      nixosConfigurations.raptor = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          catppuccin.nixosModules.catppuccin
+          home-manager.nixosModules.home-manager
+          ./hosts/raptor.nix
 
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.backupFileExtension = "backup";
-          home-manager.users.tebro = {
-            imports = [
-              ./home
-              catppuccin.homeModules.catppuccin
-            ];
-          };
-        }
-      ];
-      specialArgs = {
-        inherit inputs;
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "backup";
+            home-manager.users.tebro = {
+              imports = [ ./home catppuccin.homeModules.catppuccin ];
+            };
+          }
+        ];
+        specialArgs = { inherit inputs; };
       };
     };
-  };
 }
