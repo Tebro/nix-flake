aff88c3b284188b570e331207cef47f4807b4a77
tebro <git@tebro.simplelogin.com>
2025-05-07 13:24:18 +0300

    Add bat globally for roots alias

--- system/shell.nix
+++ system/shell.nix
@@ -1,6 +1,6 @@
-{ pkgs, ... }: {
-  environment.pathsToLink = [ "share/zsh" ];
-  environment.systemPackages = with pkgs; [ grml-zsh-config ];
+{pkgs, ...}: {
+  environment.pathsToLink = ["share/zsh"];
+  environment.systemPackages = with pkgs; [grml-zsh-config bat];
   programs.zsh = {
     enable = true;
     enableCompletion = true;
@@ -14,9 +14,6 @@
 
     interactiveShellInit = ''
       source ${pkgs.grml-zsh-config}/etc/zsh/zshrc
-
-      # Make user colour green in prompt instead of default blue
-			#zstyle ':prompt:grml:left:items:user' pre '%F{green}%B'
     '';
     promptInit = ""; # otherwise it'll override the grml prompt
   };
