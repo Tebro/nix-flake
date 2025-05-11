488a67ee5ba81b8f7f0679a86c6170f7c6ae76e7
tebro <git@tebro.simplelogin.com>
2025-05-11 23:14:48 +0300

    Enable virtualisation

--- system/virtualisation.nix
+++ system/virtualisation.nix
@@ -6,7 +6,11 @@
       dockerCompat = true;
       defaultNetwork.settings.dns_enabled = true;
     };
+    libvirtd.enable = true;
+    spiceUSBRedirection.enable = true;
   };
+  programs.virt-manager.enable = true;
+  users.groups.libvirtd.members = ["tebro"];
 
   environment.systemPackages = with pkgs; [
     docker-compose
