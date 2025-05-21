da5e24f99cc0f9387a7fade6cbd3cfa2407a9750
tebro <git@tebro.simplelogin.com>
2025-05-21 09:14:52 +0300

    zed: add tasks config

--- home/user/zed.nix
+++ home/user/zed.nix
@@ -1,8 +1,7 @@
 {config, ...}: {
-  # TODO: Enable zed with a overriden package for preview
-  # Or maybe a binary package from a flake...
   home.file = {
     ".config/zed/settings.json".source = config.lib.file.mkOutOfStoreSymlink "/home/tebro/flake/home/config/zed/settings.json";
     ".config/zed/keymap.json".source = config.lib.file.mkOutOfStoreSymlink "/home/tebro/flake/home/config/zed/keymap.json";
+    ".config/zed/tasks.json".source = config.lib.file.mkOutOfStoreSymlink "/home/tebro/flake/home/config/zed/tasks.json";
   };
 }
