aaaf1e67feec14e7013c87f766df7853f6fbdc29
tebro <git@tebro.simplelogin.com>
2025-05-26 16:52:16 +0300

    tmux: configuration

--- home/user/shell.nix
+++ home/user/shell.nix
@@ -1,32 +1,45 @@
-{
-  programs.fzf = {
-    enable = true;
-    enableZshIntegration = true;
-    enableBashIntegration = true;
-    tmux.enableShellIntegration = true;
-  };
-
-  programs.zsh = {
-    enable = true;
-    shellAliases = {
-      rebuild = "sudo nixos-rebuild --flake ~/flake switch";
-      g = "git";
+{ pkgs, ... }: {
+  programs = {
+    fzf = {
+      enable = true;
+      enableZshIntegration = true;
+      enableBashIntegration = true;
+      tmux.enableShellIntegration = true;
     };
-    enableCompletion = true;
 
-    #oh-my-zsh = {
-    #	enable = true;
-    #	theme = "robbyrussell";
-    #	plugins = [
-    #		"git"
-    #		"sudo"
-    #	];
-    #};
-    #bashrcExtra = ''
-    #  if command -v fzf-share >/dev/null; then
-    #    source "$(fzf-share)/key-bindings.bash"
-    #    source "$(fzf-share)/completion.bash"
-    #  fi
-    #'';
+    tmux = {
+      enable = true;
+      keyMode = "vi";
+      mouse = true;
+      shell = "${pkgs.zsh}/bin/zsh";
+      plugins = with pkgs.tmuxPlugins; [
+        sensible
+        cpu
+      ];
+    };
+
+    zsh = {
+      enable = true;
+      shellAliases = {
+        rebuild = "sudo nixos-rebuild --flake ~/flake switch";
+        g = "git";
+      };
+      enableCompletion = true;
+
+      #oh-my-zsh = {
+      #	enable = true;
+      #	theme = "robbyrussell";
+      #	plugins = [
+      #		"git"
+      #		"sudo"
+      #	];
+      #};
+      #bashrcExtra = ''
+      #  if command -v fzf-share >/dev/null; then
+      #    source "$(fzf-share)/key-bindings.bash"
+      #    source "$(fzf-share)/completion.bash"
+      #  fi
+      #'';
+    };
   };
 }
