74a94ac7363a115e9ad639e85ac9326854a04edc
tebro <git@tebro.simplelogin.com>
2025-05-26 12:56:35 +0300

    nvim: configure toggleterm

--- home/config/nvim/lua/tvim/ui.lua
+++ home/config/nvim/lua/tvim/ui.lua
@@ -65,6 +65,18 @@ local function init()
 		},
 	})
 
+	require("toggleterm").setup {
+		size = function(term)
+			if term.direction == "horizontal" then
+				return 15
+			elseif term.direction == "vertical" then
+				return vim.o.columns * 0.4
+			end
+		end,
+	}
+	vim.keymap.set('n', '<leader>th', '<Cmd>ToggleTerm direction=horizontal<CR>')
+	vim.keymap.set('n', '<leader>tv', '<Cmd>ToggleTerm direction=vertical<CR>')
+	vim.keymap.set('n', '<leader>tt', '<Cmd>ToggleTerm<CR>')
 end
 
 return {init = init}
