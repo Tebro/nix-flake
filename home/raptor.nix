6bb808172c9e4252527f8f8c2c7d02270260dff6
tebro <git@tebro.simplelogin.com>
2025-06-04 18:19:51 +0300

    Add opentrack-launcher utility

--- home/raptor.nix
+++ home/raptor.nix
@@ -1,4 +1,18 @@
 { pkgs, ... }: {
   # enabled in default
   programs.btop.package = pkgs.btop.override { rocmSupport = true; };
+
+  home.packages = with pkgs; [ opentrack aitrack ];
+
+  home.file = {
+    ".local/bin/opentrack-launcher" = {
+      executable = true;
+      text = ''
+        export PATH="$PATH:${pkgs.p7zip}/bin:${pkgs.opentrack}/bin"
+        echo "$@" > /home/tebro/tmp/debuglauncher.log
+        ${pkgs.python3}/bin/python3 /home/tebro/flake/home/scripts/opentrack-launcher/opentrack-launcher.py "$@" &>> /home/tebro/tmp/debuglauncher.log
+      '';
+    };
+  };
+
 }
