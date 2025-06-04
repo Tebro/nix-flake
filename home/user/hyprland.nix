476839bcc4be705636a8c217ee93939e10c99d40
tebro <git@tebro.simplelogin.com>
2025-06-04 10:38:32 +0300

    hyprland: add wallpaper

--- home/user/hyprland.nix
+++ home/user/hyprland.nix
@@ -4,33 +4,43 @@
     enable = true;
     package = pkgs.rofi-wayland;
   };
-  services.dunst.enable = true;
   programs.hyprlock.enable = true;
-  services.hypridle = {
-    enable = true;
-    settings = {
-      general = {
-        lock_cmd = "pidof hyprlock || hyprlock";
-        before_sleep_cmd = "loginctl lock-session";
-        after_sleep_cmd = "hyprctl dispatch dpms on";
-        #ignore_dbus_inhibit = false;
-      };
+  services = {
+    dunst.enable = true;
+    hypridle = {
+      enable = true;
+      settings = {
+        general = {
+          lock_cmd = "pidof hyprlock || hyprlock";
+          before_sleep_cmd = "loginctl lock-session";
+          after_sleep_cmd = "hyprctl dispatch dpms on";
+          #ignore_dbus_inhibit = false;
+        };
 
-      listener = [
-        {
-          timeout = 180;
-          on-timeout = "loginctl lock-session";
-        }
-        {
-          timeout = 220;
-          on-timeout = "hyprctl dispatch dpms off";
-          on-resume = "hyprctl dispatch dpms on";
-        }
-        {
-          timeout = 1200;
-          on-timeout = "systemctl suspend";
-        }
-      ];
+        listener = [
+          {
+            timeout = 180;
+            on-timeout = "loginctl lock-session";
+          }
+          {
+            timeout = 220;
+            on-timeout = "hyprctl dispatch dpms off";
+            on-resume = "hyprctl dispatch dpms on";
+          }
+          {
+            timeout = 1200;
+            on-timeout = "systemctl suspend";
+          }
+        ];
+      };
+    };
+    hyprpaper = {
+      enable = true;
+      settings = {
+        ipc = "on";
+        preload = [ "~/Pictures/wallpapers/sailing.jpg" ];
+        wallpaper = [ ", ~/Pictures/wallpapers/sailing.jpg" ];
+      };
     };
   };
   catppuccin = {
